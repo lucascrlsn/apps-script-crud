b3c074d3d11f1f7ce4b3526916f6c075c33d5d71
Lucas <119642350+lucascrlsn@users.noreply.github.com>
2023-10-29 06:30:43 +0000

    Update properties.js

--- properties.js
+++ properties.js
@@ -1,44 +1,47 @@
 function setProperties() {
+
   let scriptProperties = PropertiesService.getScriptProperties();
 
   // SET WORKSHEET ID
-  let SpreadsheetID = "1ASSelzFhN32EU--JyhYspcy3ZNDZmLWq1Q7Zgupdus4";
+  let SpreadsheetID = "----------------YOUR SPREADSHEET ID-----------------";
   scriptProperties.setProperty('SpreadsheetID', JSON.stringify(SpreadsheetID).replace(/['"]+/g,""));
 
   // SET COLUMN HEADER INDEXS IN THE BACKGROUND
-  let ss = SpreadsheetApp.openById(PropertiesService.getScriptProperties().getProperty('SpreadsheetID')),
-  ws = ss.getSheetByName("main"),
-  headers = ws.getDataRange().getValues().shift();
-  
+  let ss = SpreadsheetApp.openById(PropertiesService.getScriptProperties().getProperty('SpreadsheetID'));
+  let ws = ss.getSheetByName("RAW DATA");
+
+  let headers = ws.getDataRange().getValues().shift();
   // SET HEADER INDICES PROPERTY FOR LATER USE
   scriptProperties.setProperty('rawDataColIndices', JSON.stringify(headers).replace(/['"]+/g,""))
 
-  // CALL PROPERTY BY: PropertiesService.getScriptProperties().getProperty('rawDataColIndices')
+  let timestampColumnIndex = headers.indexOf("Timestamp")+1;
+  scriptProperties.setProperty('timestampColumnIndex', JSON.stringify(timestampColumnIndex).replace(/['"]+/g,""));
 
-  dateAddedColumnIndex = headers.indexOf("Date Added")+1,
-  firstNameColumnIndex = headers.indexOf("First Name")+1,
-  lastNameColumnIndex = headers.indexOf("Last Name")+1,
-  phoneNumberColumnIndex = headers.indexOf("Phone Number")+1,
-  jobTitleColumnIndex = headers.indexOf("Job Title")+1,
-  companyColumnIndex = headers.indexOf("Company")+1,
-  addressColumnIndex = headers.indexOf("Address")+1,
-  leadTypeColumnIndex = headers.indexOf("Lead Type")+1;
+  let dateColumnIndex = headers.indexOf("Date")+1;
+  scriptProperties.setProperty('dateColumnIndex', JSON.stringify(dateColumnIndex).replace(/['"]+/g,""));
 
-  try {
-    // Set multiple script properties in one call.
-    const scriptProperties = PropertiesService.getScriptProperties();
-    scriptProperties.setProperties({
-      'dateAddedColumnIndex': JSON.stringify(dateAddedColumnIndex).replace(/['"]+/g,""),
-      'firstNameColumnIndex': JSON.stringify(firstNameColumnIndex).replace(/['"]+/g,""),
-      'lastNameColumnIndex': JSON.stringify(lastNameColumnIndex).replace(/['"]+/g,""),
-      'phoneNumberColumnIndex': JSON.stringify(phoneNumberColumnIndex).replace(/['"]+/g,""),
-      'jobTitleColumnIndex': JSON.stringify(jobTitleColumnIndex).replace(/['"]+/g,""),
-      'companyColumnIndex': JSON.stringify(companyColumnIndex).replace(/['"]+/g,""),
-      'addressColumnIndex': JSON.stringify(addressColumnIndex).replace(/['"]+/g,""),
-      'leadTypeColumnIndex': JSON.stringify(leadTypeColumnIndex).replace(/['"]+/g,"")
-    });
-  } catch (err) {
-    // TODO (developer) - Handle exception
-    console.log('Failed with error %s', err.message);
-  }
+  let siteColumnIndex = headers.indexOf("Site")+1;
+  scriptProperties.setProperty('siteColumnIndex', JSON.stringify(siteColumnIndex).replace(/['"]+/g,""));
+  
+  let justificationColumnIndex = headers.indexOf("Justification")+1;
+  scriptProperties.setProperty('justificationColumnIndex', JSON.stringify(justificationColumnIndex).replace(/['"]+/g,""));
+  
+  let processColumnIndex = headers.indexOf("Process")+1;
+  scriptProperties.setProperty('processColumnIndex', JSON.stringify(processColumnIndex).replace(/['"]+/g,""));
+  
+  let employeeTypeColumnIndex = headers.indexOf("Employee Type")+1;
+  scriptProperties.setProperty('employeeTypeColumnIndex', JSON.stringify(employeeTypeColumnIndex).replace(/['"]+/g,""));
+  
+  let firstNameColumnIndex = headers.indexOf("First Name")+1;
+  scriptProperties.setProperty('firstNameColumnIndex', JSON.stringify(firstNameColumnIndex).replace(/['"]+/g,""));
+  
+  let lastNameColumnIndex = headers.indexOf("Last Name")+1;
+  scriptProperties.setProperty('lastNameColumnIndex', JSON.stringify(lastNameColumnIndex).replace(/['"]+/g,""));
+  
+  let IDColumnIndex = headers.indexOf("ID")+1;
+  scriptProperties.setProperty('IDColumnIndex', JSON.stringify(IDColumnIndex).replace(/['"]+/g,""));
+
+  let ShiftColumnIndex = headers.indexOf("Shift")+1;
+  scriptProperties.setProperty('ShiftColumnIndex', JSON.stringify(ShiftColumnIndex).replace(/['"]+/g,""));
+  
 }
